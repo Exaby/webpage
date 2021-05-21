16242d6296393cf3f47f817bfaf772d4419dc94f
Exaby <59103833+Exaby@users.noreply.github.com>
2021-05-20 20:05:35 -0400

    added links

---
+++ homepage/bookmarks.js
@@ -0,0 +1,35 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "Drive", url: "http://192.168.68.110:8080/files/" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      {
+        name: "Amazon Prime",
+        url: "https://www.amazon.com/Amazon-Video",
+      },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+    ],
+  },
+  {
+    title: "Misc",
+    links: [
+      { name: "SR Wiki", url: "https://sunrust.org" },
+      { name: "GitHub", url: "https://github.com" },
+    ],
+  },
+];
